bb30e3167e9d1e30a01956b88296dbbb98fc594d
zoompakty <zoompakty@gmail.com>
2019-01-27 18:09:22 +0200

    firsts commit

---
+++ laba_4_Client/src/sample/Controller.java
@@ -0,0 +1,263 @@
+package sample;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.*;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+    @FXML
+    private Label labelColor;
+    @FXML
+    private Label labelFigure;
+    @FXML
+    private Button buttonOpen;
+    @FXML
+    private Button buttonSave;
+    @FXML
+    private Label labelFont;
+
+    @FXML
+    private ScrollPane openPane;
+
+    @FXML
+    private TextField textFont;
+    @FXML
+    private TextField textText;
+    @FXML
+    private Label labelText;
+    @FXML
+    private Label labelH;
+
+    @FXML
+    private Label labelRad;
+
+    @FXML
+    private Label labelW;
+
+    @FXML
+    private Label labelCont;
+
+    @FXML
+    private ColorPicker cColor;
+
+    @FXML
+    private ColorPicker cContour;
+
+    @FXML
+    private ComboBox<String> cShape;
+
+    @FXML
+    private TextField textHeight;
+
+    @FXML
+    private TextField textRadius;
+
+    @FXML
+    private TextField textWidth;
+
+    public Button getButtonOpen() {
+        return buttonOpen;
+    }
+
+    public ScrollPane getOpenPane() {
+        return openPane;
+    }
+
+    public int getTextFont() {
+        return Integer.parseInt(textFont.getText());
+    }
+
+
+    public String getTextText() {
+        return textText.getText();
+    }
+
+    public int getTextRadius() {
+        return Integer.parseInt(textRadius.getText());
+    }
+
+    public String getColorToSend() {
+        return cColor.getValue().toString();
+    }
+
+    public String getStrokeToSend() {
+        return cContour.getValue().toString();
+    }
+
+    public String getShapeToSend() {
+        return cShape.getValue();
+    }
+
+    public int getTextHeight() {
+        return Integer.parseInt(textHeight.getText());
+    }
+
+    public int getTextWidth() {
+        return Integer.parseInt(textWidth.getText());
+    }
+
+    public void setTextText(String textText) {
+        this.textText.setText(textText);
+    }
+
+    public Paint getcColor() {
+        return cColor.getValue();
+    }
+
+    public Paint getcContour() {
+        return cContour.getValue();
+    }
+
+    public String getcShape() {
+        return cShape.getValue();
+    }
+
+    public Button getButtonSave() {
+        return buttonSave;
+    }
+    public void setTextRadius(int textRadius) {
+        this.textRadius.setText(String.valueOf(textRadius));    }
+    public void setTextHeight(int textHeight) {
+        this.textHeight.setText(String.valueOf(textHeight));     }
+
+    public void setTextWidth(int textWidth) {
+        this.textWidth.setText(String.valueOf(textWidth));     }
+    public void setTextFont(int textFont) {
+        this.textFont.setText(String.valueOf(textFont));
+    }
+    public void setcColor(String cColor) {
+        this.cColor.setValue(Color.valueOf(String.valueOf(cColor)));
+    }
+    public void setcStroke(String cStroke){
+        this.cContour.setValue(Color.valueOf(String.valueOf(cStroke)));
+    }
+    public void setcShape(String cShape){
+        this.cShape.setValue(String.valueOf(cShape));
+    }
+    public void hideEvrExCircle() {
+        if (cShape.getValue() == "Круг") {
+            textHeight.setVisible(false);
+            textWidth.setVisible(false);
+            labelH.setVisible(false);
+            labelW.setVisible(false);
+            labelRad.setVisible(true);
+            textRadius.setVisible(true);
+            cContour.setVisible(true);
+            textText.setVisible(false);
+            labelText.setVisible(false);
+            labelFont.setVisible(false);
+            textFont.setVisible(false);
+        } else if (cShape.getValue() == "Линия") {
+            textHeight.setVisible(false);
+            textWidth.setVisible(false);
+            labelH.setVisible(false);
+            labelW.setVisible(false);
+            labelRad.setVisible(false);
+            textRadius.setVisible(false);
+            textText.setVisible(false);
+            labelText.setVisible(false);
+            labelFont.setVisible(false);
+            textFont.setVisible(false);
+        } else if (cShape.getValue() == "Прямоугольник") {
+            textHeight.setVisible(true);
+            textWidth.setVisible(true);
+            labelH.setVisible(true);
+            labelW.setVisible(true);
+            labelRad.setVisible(false);
+            textRadius.setVisible(false);
+            textText.setVisible(false);
+            labelText.setVisible(false);
+            labelFont.setVisible(false);
+            textFont.setVisible(false);
+        } else if (cShape.getValue() == "Текст") {
+            textHeight.setVisible(false);
+            textWidth.setVisible(false);
+            labelH.setVisible(false);
+            labelW.setVisible(false);
+            labelRad.setVisible(false);
+            textRadius.setVisible(false);
+            textText.setVisible(true);
+            labelText.setVisible(true);
+            labelFont.setVisible(true);
+            textFont.setVisible(true);
+
+        }
+    }
+public void hideAll(){
+        labelFont.setVisible(false);
+        labelText.setVisible(false);
+        labelRad.setVisible(false);
+        labelW.setVisible(false);
+        labelH.setVisible(false);
+        labelColor.setVisible(false);
+        labelCont.setVisible(false);
+        labelFigure.setVisible(false);
+        cShape.setVisible(false);
+        cContour.setVisible(false);
+        cColor.setVisible(false);
+        textText.setVisible(false);
+        textWidth.setVisible(false);
+        textHeight.setVisible(false);
+        textRadius.setVisible(false);
+        textFont.setVisible(false);
+        buttonSave.setVisible(false);
+}
+public void unHideAll(){
+    labelFont.setVisible(true);
+    labelText.setVisible(true);
+    labelRad.setVisible(true);
+    labelW.setVisible(true);
+    labelH.setVisible(true);
+    labelColor.setVisible(true);
+    labelCont.setVisible(true);
+    labelFigure.setVisible(true);
+    cShape.setVisible(true);
+    cContour.setVisible(true);
+    cColor.setVisible(true);
+    textText.setVisible(true);
+    textWidth.setVisible(true);
+    textHeight.setVisible(true);
+    textRadius.setVisible(true);
+    textFont.setVisible(true);
+    buttonSave.setVisible(true);
+
+}
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        textHeight.setVisible(false);
+        textWidth.setVisible(false);
+        textRadius.setVisible(false);
+        labelH.setVisible(false);
+        labelW.setVisible(false);
+        labelRad.setVisible(false);
+        textText.setVisible(false);
+        labelText.setVisible(false);
+        labelFont.setVisible(false);
+        textFont.setVisible(false);
+        openPane.setVisible(false);
+
+        cShape.getItems().addAll("Прямоугольник", "Круг", "Текст", "Линия");
+        textRadius.setText("10");
+        textWidth.setText("10");
+        textHeight.setText("10");
+
+
+    }
+}
---
+++ laba_4_Client/src/sample/Main.java
@@ -0,0 +1,346 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+public class Main extends Application {
+
+    double endX, endY;
+    double locX;
+    double locY;
+    InputStream is;
+    DataInputStream dis;
+
+    Thread thread;
+    Thread thread1;
+    ServerSocket s2;
+    Socket ss2;
+    ImageView myImageView = new ImageView();
+
+    String shapeValue;
+    String colorValue;
+    String strokeValue;
+    double checkCircleX = 0.11111;
+    double checkCircleY = 0.11111;
+    double checkSquareX = 0.11111;
+    double checkSquareY = 0.11111;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml")
+        );
+        Parent root = loader.load();
+        Controller controller = (Controller) loader.getController();
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Socket s = new Socket("localhost", 3000);
+
+        OutputStream os = s.getOutputStream();
+        OutputStream osp = s.getOutputStream();
+
+        DataOutputStream dos = new DataOutputStream(os);
+        is = s.getInputStream();
+        dis = new DataInputStream(is);
+
+
+
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    if ((controller.getcShape() == "Линия")) {
+                        System.out.println(event.getSceneX());
+                        System.out.println(event.getSceneY());
+                        endX = event.getSceneX();
+                        endY = event.getSceneY();
+                        Line line = new Line(locX, locY, endX, endY);
+                        line.setStroke(controller.getcColor());
+                        ((Pane) root).getChildren().add(line);
+                        try {
+                            dos.writeDouble(locX);
+                            dos.writeDouble(locY);
+                            dos.writeUTF(controller.getShapeToSend());
+                            dos.writeUTF(controller.getColorToSend());
+                            dos.writeUTF(controller.getStrokeToSend());
+                            dos.writeDouble(endX);
+                            dos.writeDouble(endY);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    System.out.println(event.getSceneX());
+                    System.out.println(event.getSceneY());
+                    locX = event.getSceneX();
+                    locY = event.getSceneY();
+
+                    if (controller.getcShape() == "Круг") {
+                        Circle circle = new Circle(locX, locY, controller.getTextRadius());
+                        circle.setFill(controller.getcColor());
+                        circle.setStrokeWidth(controller.getTextRadius() / 3);
+                        circle.setStroke(controller.getcContour());
+                        ((Pane) root).getChildren().add(circle);
+                        try {
+                            dos.writeDouble(locX);
+                            dos.writeDouble(locY);
+                            dos.writeUTF(controller.getShapeToSend());
+                            dos.writeUTF(controller.getColorToSend());
+                            dos.writeUTF(controller.getStrokeToSend());
+                            dos.writeInt(controller.getTextRadius());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (controller.getcShape() == "Прямоугольник") {
+                        Rectangle rectangle = new Rectangle(locX, locY, controller.getTextWidth(), controller.getTextHeight());
+                        rectangle.setFill(controller.getcColor());
+                        rectangle.setStrokeWidth(controller.getTextRadius() / 5);
+                        rectangle.setStroke(controller.getcContour());
+                        ((Pane) root).getChildren().add(rectangle);
+                        try {
+                            dos.writeDouble(locX);
+                            dos.writeDouble(locY);
+                            dos.writeUTF(controller.getShapeToSend());
+                            dos.writeUTF(controller.getColorToSend());
+                            dos.writeUTF(controller.getStrokeToSend());
+                            dos.writeInt(controller.getTextHeight());
+                            dos.writeInt(controller.getTextWidth());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (controller.getcShape() == "Текст") {
+                        Text text = new Text(locX, locY, controller.getTextText());
+                        text.setFont(new Font(controller.getTextFont()));
+                        text.setFill(controller.getcColor());
+                        ((Pane) root).getChildren().add(text);
+                        try {
+                            dos.writeDouble(locX);
+                            dos.writeDouble(locY);
+                            dos.writeUTF(controller.getShapeToSend());
+                            dos.writeUTF(controller.getColorToSend());
+                            dos.writeUTF(controller.getStrokeToSend());
+                            dos.writeUTF(controller.getTextText());
+                            dos.writeInt(controller.getTextFont());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+
+        });
+        Button saveButton = controller.getButtonSave();
+        saveButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent e) {
+                        controller.hideAll();
+                        WritableImage image = root.snapshot(new SnapshotParameters(), null);
+
+                        // TODO: probably use a file chooser here
+                        FileChooser fileChooser = new FileChooser();
+                        fileChooser.setTitle("Save Image");
+                        File file = fileChooser.showSaveDialog(primaryStage);
+                        try {
+                            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+                        } catch (IOException r) {
+                            // TODO: handle exception here
+                        }
+                        controller.unHideAll();
+                    }
+                });
+        ScrollPane scrollPane = controller.getOpenPane();
+        Button openButton = controller.getButtonOpen();
+        openButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent e) {
+                        Socket s2 = null;
+                        OutputStream outputStream=null;
+                        try {
+                            s2 = new Socket("localhost", 2000);
+                            outputStream = s2.getOutputStream();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+
+                        ImageView myImageView = new ImageView();
+                        FileChooser fileChooser = new FileChooser();
+                        //Set extension filter
+                        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+                        FileChooser.ExtensionFilter extFilterJPG =
+                                new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG", "*.jpg",
+                                        "*.JPEG", "*.jpeg");
+                        fileChooser.getExtensionFilters().addAll(extFilterPNG,extFilterJPG);
+                        //Show open file dialog
+                        File file = fileChooser.showOpenDialog(null);
+
+                        try {
+                            BufferedImage bufferedImage = ImageIO.read(file);
+                            ImageIO.write(bufferedImage,"png",s2.getOutputStream());
+                            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                            myImageView.setImage(image);
+                            s2.close();
+                        } catch (IOException ex) {
+                        }
+                        scrollPane.setContent(myImageView);
+                        scrollPane.setVisible(true);
+                    }
+                });
+
+        thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                InputStream inputStream = null;
+                while (true) {
+                    try {
+                        s2 = new ServerSocket(4000);
+                        System.out.println("openServerSocket is created");
+                        ss2 = s2.accept();
+                        System.out.println("openServerSocket is accepted");
+
+                        BufferedImage bufferedImage = ImageIO.read(ImageIO.createImageInputStream(ss2.getInputStream()));
+                        /*ImageIO.write(bufferedImage, "png", new File("C:\\Users\\danil\\Desktop\\test2.png"));//Удалить строчку на другом ПК*/
+                        s2.close();
+                        Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                        myImageView.setImage(image);
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                scrollPane.setContent(myImageView);
+                                scrollPane.setVisible(true);
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+        thread1.start();
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        locX = dis.readDouble();
+                        locY = dis.readDouble();
+                        shapeValue = dis.readUTF();
+                        System.out.println("Figure: " + shapeValue);
+                        colorValue = dis.readUTF();
+                        System.out.println("Color: " + colorValue);
+                        strokeValue = dis.readUTF();
+                        System.out.println("Color of stroke : " + strokeValue);
+
+                        System.out.println(locX);
+                        System.out.println(locY);
+                        switch (shapeValue) {
+                            case "Круг":
+                                controller.setTextRadius(dis.readInt());
+                                break;
+                            case "Прямоугольник":
+                                controller.setTextHeight(dis.readInt());
+                                controller.setTextWidth(dis.readInt());
+                                break;
+                            case "Текст":
+                                controller.setTextText(dis.readUTF());
+                                controller.setTextFont(dis.readInt());
+                                break;
+                            case "Линия":
+                                endX = dis.readDouble();
+                                endY = dis.readDouble();
+                                break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            controller.setcShape(shapeValue);
+                            controller.setcColor(colorValue);
+                            controller.setcStroke(strokeValue);
+                            if (shapeValue.equals("Круг")) {
+                                Circle circle = new Circle(locX, locY, controller.getTextRadius());
+                                circle.setFill(controller.getcColor());
+                                circle.setStrokeWidth(controller.getTextRadius() / 3);
+                                circle.setStroke(controller.getcContour());
+                                ((Pane) root).getChildren().add(circle);
+
+                            } else if (shapeValue.equals("Прямоугольник")) {
+                                if (checkSquareX != locX && checkSquareY != locY) {
+                                    Rectangle rectangle = new Rectangle(locX, locY, controller.getTextWidth(), controller.getTextHeight());
+                                    rectangle.setFill(controller.getcColor());
+                                    rectangle.setStrokeWidth(controller.getTextRadius() / 5);
+                                    rectangle.setStroke(controller.getcContour());
+                                    ((Pane) root).getChildren().add(rectangle);
+                                    checkSquareX = locX;
+                                    checkSquareY = locY;
+                                }
+                            } else if (shapeValue.equals("Текст")) {
+                                Text text = new Text(locX, locY, controller.getTextText());
+                                text.setFont(new Font(controller.getTextFont()));
+                                text.setFill(controller.getcColor());
+                                ((Pane) root).getChildren().add(text);
+                            } else if (shapeValue.equals("Линия")) {
+                                Line line = new Line(locX, locY, endX, endY);
+                                line.setStroke(controller.getcColor());
+                                ((Pane) root).getChildren().add(line);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        thread.start();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
